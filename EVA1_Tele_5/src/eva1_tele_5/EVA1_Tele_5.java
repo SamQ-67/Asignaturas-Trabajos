366332106a232e1fa9b0daf2b374fce302b7d321
SamQ-67 <ririosazthurex@gmail.com>
2025-02-07 18:53:38 -0600

    Add files via upload

---
+++ EVA1_Tele_5/src/eva1_tele_5/EVA1_Tele_5.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_tele_5;
+
+/**
+ *
+ * @author ririo
+ */
+public class EVA1_Tele_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Tv_Clase Tv = new Tv_Clase();
+        Tv.imprimirDatos();
+        Tv.encenderApagar();//BOTON DE ENCENDIDO DEL CONTROL
+        Tv.subirVolumen();
+        Tv.masCanal();
+        Tv.masCanal();
+        Tv.masCanal();
+        Tv.masCanal();
+        Tv.masCanal();
+        Tv.masCanal();
+        Tv.masCanal();
+        Tv.masCanal();
+        Tv.masCanal();
+        Tv.imprimirDatos();
+    }
+    
+}
